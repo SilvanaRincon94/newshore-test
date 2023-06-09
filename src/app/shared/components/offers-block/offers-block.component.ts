984e2a4e6aad2d140334213390d2f29ba01b514d
Silvana Andrea Rincon Zapata <79461605+silvi94ayjek@users.noreply.github.com>
2023-06-09 10:54:26 -0500

    new model, complementary information for images and comments in code

--- src/app/shared/components/offers-block/offers-block.component.ts
+++ src/app/shared/components/offers-block/offers-block.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// This is the url for the model of cards
+import { CardPlace } from 'src/app/shared/models/card';
 
 @Component({
   selector: 'app-offers-block',
@@ -12,4 +14,28 @@ export class OffersBlockComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // This is the array for filling in the information on the cards.
+  cardPlace: CardPlace[] = [
+    {
+      image: '/assets/images/chicago.jpg',
+      imageWidth: 1248,
+      imageHeight: 767,
+      city: 'Chicago',
+      price: '$600.00',
+    },
+    {
+      image: '/assets/images/porto.jpg',
+      imageWidth: 1248,
+      imageHeight: 929,
+      city: 'Porto',
+      price: '$600.00',
+    },
+    {
+      image: '/assets/images/mire.jpg',
+      imageWidth: 1248,
+      imageHeight: 729,
+      city: 'Chicago',
+      price: '$600.00',
+    },
+  ]
 }
---
+++ src/app/shared/models/card.ts
@@ -0,0 +1,8 @@
+// This is the model of cards for offers section
+export interface CardPlace {
+  image: string;
+  imageWidth: number;
+  imageHeight: number;
+  city: string;
+  price: string;
+}
